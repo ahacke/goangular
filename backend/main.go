701fffd7adc70e7e197d05e84d5d198303f4fe99
ahacke <hacke.andre@gmail.com>
2018-05-26 21:47:11 +0200

    Add sqlite with test code

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -11,6 +16,84 @@ func main() {
 
 	http.HandleFunc("/api/hello", helloWorld)
 	http.ListenAndServe(":8080", nil)
+
+	// sqlite
+
+	// table
+	/*
+			CREATE TABLE `userinfo` (
+		        `uid` INTEGER PRIMARY KEY AUTOINCREMENT,
+		        `username` VARCHAR(64) NULL,
+		        `departname` VARCHAR(64) NULL,
+		        `created` DATE NULL
+			);
+	*/
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+
+	// insert
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+	// update
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	// query
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+	var uid int
+	var username string
+	var department string
+	var created time.Time
+
+	for rows.Next() {
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	rows.Close() //good habit to close
+
+	// delete
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Readme
 
 Backend must be launched from the goangular root directory.
+
+## Installation
+
+Follow this:
+https://github.com/mattn/go-sqlite3/issues/212#issuecomment-273531789
