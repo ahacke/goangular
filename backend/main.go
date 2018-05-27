5be0ecaf2bfe09f626767f9356bb3faa1a3259de
ahacke <hacke.andre@gmail.com>
2018-05-27 17:29:35 +0200

    Add ReST endpoints using sqlite to store data

--- backend/main.go
+++ backend/main.go
@@ -11,11 +11,86 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type entryStruct struct {
+	Username   string
+	Departname string
+	Created    int64
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./app/dist/angular-app")))
 
 	http.HandleFunc("/api/hello", helloWorld)
+	http.HandleFunc("/api/entry", postEntry)
+	http.HandleFunc("/api/entries", getEntries)
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func getEntries(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM userinfo")
+	defer rows.Close()
+	checkErr(err)
+
+	data := []entryStruct{}
+	var entry entryStruct
+	var id int
+
+	for rows.Next() {
+		err = rows.Scan(&id, &entry.Username, &entry.Departname, &entry.Created)
+		checkErr(err)
+		data = append(data, entry)
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Println(err)
+	}
+
+}
+
+func postEntry(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+	defer db.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	var t entryStruct
+	err = decoder.Decode(&t)
+	checkErr(err)
+	defer r.Body.Close()
+	log.Printf("%+v\n", t)
+
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+	checkErr(err)
+
+	_, err = stmt.Exec(t.Username, t.Departname, t.Created)
+	checkErr(err)
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Message string
+	}{
+		"Hello, World",
+	}
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Println(err)
+	}
+}
+
+func sqliteTest() {
 
 	// sqlite
 
@@ -87,23 +162,10 @@ func main() {
 	fmt.Println(affect)
 
 	db.Close()
-
 }
 
 func checkErr(err error) {
 	if err != nil {
 		panic(err)
-	}
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	data := struct {
-		Message string
-	}{
-		"Hello, World",
-	}
-
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Println(err)
 	}
 }
