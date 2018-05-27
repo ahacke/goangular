434b60c25d1b84f4228cce13da40df291a276dfc
ahacke <hacke.andre@gmail.com>
2018-05-27 19:19:45 +0200

    rename, database initialization, embed app files in build

--- goangular.go
+++ goangular.go
@@ -3,12 +3,16 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 
+	"github.com/GeertJohan/go.rice"
 	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	applicationName  string = "goangular"
+	databaseFileName string = applicationName + ".db"
 )
 
 type entryStruct struct {
@@ -18,20 +22,35 @@ type entryStruct struct {
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./app/dist/angular-app")))
+	initializeDatabase()
+
+	http.Handle("/", http.FileServer(rice.MustFindBox("angular-app").HTTPBox()))
 
 	http.HandleFunc("/api/hello", helloWorld)
 	http.HandleFunc("/api/entry", postEntry)
 	http.HandleFunc("/api/entries", getEntries)
 	http.ListenAndServe(":8080", nil)
+}
 
+func initializeDatabase() {
+	db, err := sql.Open("sqlite3", "./"+databaseFileName)
+	checkErr(err)
+	defer db.Close()
+
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS userinfo (uid INTEGER PRIMARY KEY AUTOINCREMENT,username VARCHAR(64) NULL,departname VARCHAR(64) NULL,created INTEGER NULL)")
+	checkErr(err)
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func getEntries(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusBadRequest)
 	}
-	db, err := sql.Open("sqlite3", "./foo.db")
+	db, err := sql.Open("sqlite3", "./"+databaseFileName)
 	checkErr(err)
 	defer db.Close()
 
@@ -51,14 +70,13 @@ func getEntries(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Println(err)
 	}
-
 }
 
 func postEntry(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusBadRequest)
 	}
-	db, err := sql.Open("sqlite3", "./foo.db")
+	db, err := sql.Open("sqlite3", "./"+databaseFileName)
 	checkErr(err)
 	defer db.Close()
 
@@ -88,80 +106,6 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Println(err)
 	}
-}
-
-func sqliteTest() {
-
-	// sqlite
-
-	// table
-	/*
-			CREATE TABLE `userinfo` (
-		        `uid` INTEGER PRIMARY KEY AUTOINCREMENT,
-		        `username` VARCHAR(64) NULL,
-		        `departname` VARCHAR(64) NULL,
-		        `created` DATE NULL
-			);
-	*/
-
-	db, err := sql.Open("sqlite3", "./foo.db")
-	checkErr(err)
-
-	// insert
-	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
-	checkErr(err)
-
-	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
-	checkErr(err)
-
-	id, err := res.LastInsertId()
-	checkErr(err)
-
-	fmt.Println(id)
-	// update
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec("astaxieupdate", id)
-	checkErr(err)
-
-	affect, err := res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-
-	// query
-	rows, err := db.Query("SELECT * FROM userinfo")
-	checkErr(err)
-	var uid int
-	var username string
-	var department string
-	var created time.Time
-
-	for rows.Next() {
-		err = rows.Scan(&uid, &username, &department, &created)
-		checkErr(err)
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
-	}
-
-	rows.Close() //good habit to close
-
-	// delete
-	stmt, err = db.Prepare("delete from userinfo where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec(id)
-	checkErr(err)
-
-	affect, err = res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-
-	db.Close()
 }
 
 func checkErr(err error) {
